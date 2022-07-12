a5daf41376cf960f7c021d13938a28e167e5f4d4
Nizhixin <18831498100@163.com>
2022-07-12 10:14:32 +0800

    新建

---
+++ git001_javase/src/main/java/com/powernode/demo/Test02.java
@@ -0,0 +1,10 @@
+package com.powernode.demo;
+
+import java.sql.SQLOutput;
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("jdklasjfas");
+    }
+}
+
